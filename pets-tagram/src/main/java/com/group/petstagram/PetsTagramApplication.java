46d35a416d69f10f343d6a64f3450fbff27282cb
Mikayel <86086637+Mikaye1@users.noreply.github.com>
2022-02-15 20:06:00 -0800

    Added Petstagram Project

---
+++ pets-tagram/src/main/java/com/group/petstagram/PetsTagramApplication.java
@@ -0,0 +1,13 @@
+package com.group.petstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsTagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsTagramApplication.class, args);
+	}
+
+}
---
+++ pets-tagram/src/test/java/com/group/petstagram/PetsTagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.petstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsTagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
